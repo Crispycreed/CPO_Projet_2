afaed064e70ce1f342c3196004e4fd8689e9f870
Denis MAUMEJEAN <denis.maumejean@epfedu.fr>
2023-11-22 13:14:27 +0100

    suite

--- Projet_2/src/GrilleDeJeu.java
+++ Projet_2/src/GrilleDeJeu.java
@@ -44,47 +44,7 @@ class GrilleDeJeu {
         }
     }
 
-    public void deplacerToutesCellulesDroite() {
-        for (int i = 0; i < nbLignes; i++) {
-            for (int j = nbColonnes - 2; j >= 0; j--) {
-                deplacerCelluleDroite(i, j);
-            }
-        }
-    }
-
-    private void deplacerCelluleDroite(int ligne, int colonne) {
-        if (!matriceCellules[ligne][colonne].estVide()) {
-            int valeurCourante = matriceCellules[ligne][colonne].getValeur();
-
-            // Vérifier si la cellule peut se déplacer vers la droite
-            if (colonne + 1 < nbColonnes && matriceCellules[ligne][colonne + 1].estVide()) {
-                // Déplacer la cellule vers la droite
-                matriceCellules[ligne][colonne + 1].modifierValeur(valeurCourante);
-                matriceCellules[ligne][colonne].modifierValeur(0);
-            }
-        }
-    }
-
-public void deplacerToutesCellulesHaut() {
-    for (int i = 1; i < nbLignes; i++) {
-        for (int j = 0; j < nbColonnes; j++) {
-            deplacerCelluleHaut(i, j);
-        }
-    }
-}
-
-private void deplacerCelluleHaut(int ligne, int colonne) {
-    if (!matriceCellules[ligne][colonne].estVide()) {
-        int valeurCourante = matriceCellules[ligne][colonne].getValeur();
-
-        // Vérifier si la cellule peut se déplacer vers le haut
-        if (ligne - 1 >= 0 && matriceCellules[ligne - 1][colonne].estVide()) {
-            // Déplacer la cellule vers le haut
-            matriceCellules[ligne - 1][colonne].modifierValeur(valeurCourante);
-            matriceCellules[ligne][colonne].modifierValeur(0);
-        }
-    }
-}
+public void AdditionnerToutesCellulesValideVersLaDroite() {
 
 
 }
